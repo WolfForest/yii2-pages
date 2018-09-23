3bc6f287b29fa6783f6d65fa66458d0b62ee678e
forest <wolfrnd@ya.ru>
2018-09-24 02:18:45 +0500

    start

---
+++ PageModule.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Forest
+ * Date: 23.09.2018
+ * Time: 5:33
+ */
+
+namespace forest\page;
+
+
+use yii\base\Module;
+
+class PageModule extends Module
+{
+
+    public $controllerNamespace = 'forest\page\controllers';
+
+    /**
+     * @p
+     */
+    public function init()
+    {
+        parent::init();
+
+    }
+}
